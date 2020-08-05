0ed57c029c5b9bce45e8c0f199ccf16a5a257195
amir-shariati <seyed.amir.shariati@gmail.com>
2020-08-05 13:12:46 +0430

    camera controller is updated

--- src/components/cameraController/index.js
+++ src/components/cameraController/index.js
@@ -1,16 +1,30 @@
-import {useThree} from "react-three-fiber";
+import React, {useRef} from "react";
+import {useFrame, useThree} from "react-three-fiber";
 import {useEffect} from "react";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 
+
 export const CameraController = () => {
     const { camera, gl } = useThree();
+    const controls = new OrbitControls(camera, gl.domElement);
+    controls.minDistance = 2;
+    controls.maxDistance = 9;
+    controls.target.set(0,0,0); // option
+    controls.enableDamping = true; // option
+    controls.dampingFactor = 0.05;
     useEffect(
         () => {
-            const controls = new OrbitControls(camera, gl.domElement);
+            // const controls = new OrbitControls(camera, gl.domElement);
+            //
+            // controls.minDistance = 3;
+            // controls.maxDistance = 20;
+            //
+            // controls.target.set(0,0,0); // option
+            // controls.enableDamping = true; // option
+            // required if controls.enableDamping or controls.autoRotate are set to true
+            controls.update();
 
-            controls.minDistance = 3;
-            controls.maxDistance = 20;
             return () => {
                 controls.dispose();
             };
